f50adea96b82252024790c599f78312d4210017c
solomonadzape95 <adzapesolomon@gmail.com>
2025-12-21 08:29:41 +0100

    feat: updated color

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <div className="w-screen h-screen bg-white pointer-events-none"></div>
   </StrictMode>,
 )
